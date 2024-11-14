2b03317c1829ca20639a6362457cccf220eb64ba
sayedkhalil88 <sayedkhalil888000@gmail.com>
2024-11-14 18:46:05 +0200

    Add files via upload

---
+++ Properties/properties.project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace properties.project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dollar dollar =new Dollar(1.34m);
+            dollar.Amount = 1;
+            Console.WriteLine(dollar.Amount);
+            
+        }
+    }
+
+    public class Dollar
+    {
+        private decimal _amount; 
+        public decimal Amount
+        {
+             get { return _amount;
+            }
+            set { 
+                // validation 
+               this._amount = ProcessValue(value);
+            }
+        }
+        public Dollar(decimal amount  ) {
+           this._amount = ProcessValue( amount );
+        }
+        public decimal ProcessValue(decimal value) => value < 0 ? 0 :Math.Round ( value , 2);  
+    }
+    
+}
