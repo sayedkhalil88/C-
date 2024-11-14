a6108a71077930c03ae5f2f4d51e950b6d1ac3f6
sayedkhalil88 <sayedkhalil888000@gmail.com>
2024-11-14 18:41:11 +0200

    Add files via upload

---
+++ methods/methods.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace methods._1
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            demo obj = new demo();
+
+            obj.printAllEven(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }); 
+            //int res = obj.sum(out num); 
+            //  Console.WriteLine(res);
+            //Console.WriteLine(num);    
+            // Console.ReadKey();  
+        }
+    }
+  public class demo
+    {
+       public void printAllEven(int [] arr)
+        {
+            foreach (var i in  arr){
+                if (IsEven(i)) {
+                    Console.WriteLine(i);
+                }
+            }
+             bool IsEven(int num) => num % 2 == 0;
+
+        }
+       
+    }
+}
