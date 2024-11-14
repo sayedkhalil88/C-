d484610be7debecfbb3dec0cb3984846d36caab5
sayedkhalil88 <sayedkhalil888000@gmail.com>
2024-11-14 18:43:37 +0200

    Add files via upload

---
+++ Conv&cast/conv&cast.p/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace conv_cast.p
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] s = { "49", "73", "73", "61", "6D" };
+            foreach (var hex in s)
+            {
+                int value = Convert.ToInt32(hex , 16 );
+                string hexChar = char.ConvertFromUtf32(value);
+                var ch =(char) value ;  
+                Console.Write(hexChar) ;
+                Console.WriteLine(ch);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
