65f0de737a222cc50195824ba63a436d86297742
sayedkhalil88 <sayedkhalil888000@gmail.com>
2024-11-14 18:42:37 +0200

    Add files via upload

---
+++ OOP/OOP.1/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._1
+{
+    public class Employee
+    {
+        public static double TAX = 0.03;  
+        public string fname;
+        public string lname;
+        public double wage;
+        public double loggedHours;
+
+        private double Calcukate() =>  wage* loggedHours;
+        private double CalculateTAX() => Calcukate() * TAX; 
+        private double CalculateNetSalary () => Calcukate() -CalculateTAX();
+
+        public string PrintAll()
+        {
+            return ($"\n first name is {fname}") +
+                   ($"\n last name is {lname}") +
+                   ($"\n the wage = {wage}") +
+                   ($"\n the logged hours = {loggedHours}") +
+                   ($"\n---------------------------------- !")+
+                   ($"\n the salary = {Calcukate()}") +
+                   ($"\n the tax = {CalculateTAX()}") +
+                   ($"\n the net salary = {CalculateNetSalary()}");
+
+        }
+
+    } 
+     
+   
+}
---
+++ OOP/OOP.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OOP._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] allEmployees = new Employee[2];
+            Employee emp1 =new Employee();
+            Employee emp2 =new Employee();
+
+            Console.Write("first name : ");
+            emp1.fname =Console.ReadLine();
+            Console.Write("last name : ");
+            emp1.lname = Console.ReadLine();
+            Console.Write("wage : ");
+            emp1.wage = Convert.ToDouble(Console.ReadLine());   
+            Console.Write("LOgged Hours : ");
+            emp1.loggedHours=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the TAX :  ");
+            Employee.TAX = Convert.ToDouble(Console.ReadLine()); 
+            
+            allEmployees [0] = emp1;
+            allEmployees[1] = emp2;
+
+            Console.Write("first name : ");
+            emp2.fname = Console.ReadLine();
+            Console.Write("last name : ");
+            emp2.lname = Console.ReadLine();
+            Console.Write("wage : ");
+            emp2.wage = Convert.ToDouble(Console.ReadLine());
+            Console.Write("LOgged Hours : ");
+            emp2.loggedHours = Convert.ToDouble(Console.ReadLine());
+          
+            foreach (Employee emp in allEmployees)
+            {
+               Console.WriteLine(emp.PrintAll());
+               
+            }
+
+        }
+    }
+}
