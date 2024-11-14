273a1166434c1eb8318debb5afc20c32b09e7750
sayedkhalil88 <sayedkhalil888000@gmail.com>
2024-11-14 18:39:28 +0200

    Add files via upload

---
+++ Array/Array.project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;  
+namespace Array.project
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3 }; 
+            foreach(var i in arr ){
+                Console.WriteLine(i);
+            }
+          
+        }
+    }
+}
